baeb9f764604d6c8179ec116e8d3e1a571b588fe
Nigouu <uggla.nicolas@gmail.com>
2021-12-08 12:51:35 +0200

    login and validation works

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,10 +1,91 @@
 import React from 'react';
-import { Text, TextInput, Pressable, View, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+  Button,
+  Pressable
+} from 'react-native'
 import { Formik, useField } from 'formik';
-import * as yup from 'yup';
+import * as yup from 'yup'
+
+const loginValidationSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Please enter valid email")
+    .required('Email Address is Required'),
+  password: yup
+    .string()
+    .min(8, ({ min }) => `Password must be at least ${min} characters`)
+    .required('Password is required'),
+})
+
+const SignIn = () => {
+  return (
+    <View style={styles.loginContainer}>
+          <Text>Login Screen</Text>
+          <Formik
+            validateOnMount={true}
+            validationSchema={loginValidationSchema}
+            initialValues={{ email: '', password: '' }}
+            onSubmit={values => console.log(values)}
+          >
+            {({ handleChange,
+              handleBlur,
+              handleSubmit,
+              values,
+              errors,
+              touched,
+              isValid, }) => (
+              <>
+                <TextInput
+                  name="email"
+                  placeholder="Email Address"
+                  style={styles.textInput}
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                />
+                {(errors.email && touched.email) &&
+                  <Text style={styles.errorText}>{errors.email}</Text>
+                }
+
+                <TextInput
+                  name="password"
+                  placeholder="Password"
+                  style={styles.textInput}
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  secureTextEntry
+                />
+                {(errors.password && touched.password) &&
+                  <Text style={styles.errorText}>{errors.password}</Text>
+                }
+                <Pressable onPress={handleSubmit}
+                  title="LOGIN"
+                  disabled={!isValid || values.email === ''} > 
+                  <Text>Sign In</Text>
+                </Pressable>
+              </>
+            )}
+          </Formik>
+        </View>
+  )
+};
 
 const styles = StyleSheet.create({
 
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   loginContainer: {
     width: '80%',
     alignItems: 'center',
@@ -22,56 +103,10 @@ const styles = StyleSheet.create({
     borderWidth: StyleSheet.hairlineWidth,
     borderRadius: 10,
   },
+  errorText: {
+    fontSize: 10,
+    color: 'red',
+  },
 })
-
-const SignIn = () => {
-  const validationSchema = yup.object().shape({
-    email: yup.string()
-      .min(2, 'Too Short!')
-      .max(15, 'Too long!')
-      .required('Required'),
-    password: yup.string()
-      .min(2, 'Too short!')
-      .max(15, 'Too long!')
-      .required('Required'),
-  });
-
-  return (
-    <View style={styles.loginContainer}>
-          <Text>Login Screen</Text>
-          <Formik
-            initialValues={{ email: '', password: '' }}
-            onSubmit={values => console.log(values)}
-            validationSchema={validationSchema}
-          >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
-              <>
-                <TextInput
-                  name="email"
-                  placeholder="Email Address"
-                  style={styles.textInput}
-                  onChangeText={handleChange('email')}
-                  onBlur={handleBlur('email')}
-                  value={values.email}
-                  keyboardType="email-address"
-                />
-                <TextInput
-                  name="password"
-                  placeholder="Password"
-                  style={styles.textInput}
-                  onChangeText={handleChange('password')}
-                  onBlur={handleBlur('password')}
-                  value={values.password}
-                  secureTextEntry
-                />
-                <Pressable onPress={handleSubmit}>
-                  <Text>Submit me mothafucka!!</Text>
-                </Pressable>
-              </>
-            )}
-          </Formik>
-        </View>
-  )
-};
 
 export default SignIn;
